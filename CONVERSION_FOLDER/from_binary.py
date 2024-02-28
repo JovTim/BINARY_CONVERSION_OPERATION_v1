09c170ea2d8f6dd8964006db4c0ce82af8da65ca
JovTim <202280218@psu.palawan.edu.ph>
2024-02-28 12:12:38 +0800

    from binary to x

--- CONVERSION_FOLDER/from_binary.py
+++ CONVERSION_FOLDER/from_binary.py
@@ -1,32 +1,28 @@
-from decimal_to_binary import decimal_to_binary
+from binary_to_decimal import binary_to_decimal
 from twos_complement import final_phase
 from splitter import splitter, hex_replacer
-from binary_to_decimal import binary_to_decimal
 
-user = '-20.5'
+user = "100111.11"
 
-def signed_deci(value):
-    decimal = ""
-    for i in value:
-        if i == '-':
-            continue
-        decimal += i
+def signed_binary(value):
+    #-------------SIGNED DECIMAL---------
+    x = final_phase(value)
+    decimal = binary_to_decimal(x)
 
-    x = decimal_to_binary(decimal)
-    deci = final_phase(x) # signed binary
-    # try pa mag add ng 1111 sa dulo
-    print(f"Binary: {deci}")
-    #return x, deci
+    binary = value
 
-    #---------------------SIGNED OCTAL---------------
-    oc_binary_whole, oc_binary_frac = splitter(deci)
+    #return "-" + str(decimal), binary
+    print(f"DECIMAL: -{decimal}")
+    #-------------SIGNED OCTAL-----------
+
+    oc_binary_whole, oc_binary_frac = splitter(binary)
     oc_whole_container = []
     oc = ""
     for i in range(1, len(oc_binary_whole) + 1):
         if len(oc) == 3:
             oc_whole_container.append(oc)
             oc = ""
-        oc = str(oc_binary_whole[-i]) + oc
+        oc = str(oc_binary_whole[-i]) + oc 
     
     if oc:
         for _ in range(3):
@@ -34,15 +30,12 @@ def signed_deci(value):
                 oc_whole_container.append(oc)
                 break
             oc = '1' + oc
-    
-    octal_whole =""
 
+    octal_whole = ""
     for i in oc_whole_container:
         y = binary_to_decimal(i)
         octal_whole = str(y) + octal_whole
-    
-    #return octal_whole
-        
+
     oc_fraction_container = []
     oc_frac = ""
     for j in oc_binary_frac:
@@ -50,7 +43,7 @@ def signed_deci(value):
             oc_fraction_container.append(oc_frac)
             oc_frac = ""
         oc_frac += str(j)
-
+    
     if oc_frac:
         for _ in range(3):
             if len(oc_frac) == 3:
@@ -63,12 +56,9 @@ def signed_deci(value):
         a = binary_to_decimal(j)
         octal_fraction += str(a)
 
-    
-    #return octal_whole + octal_fraction
     print(f"OCTAL: {octal_whole + octal_fraction}")
-    
-    #----------------------SIGNED HEXADECIMAL----------------
-    he_binary_whole, he_binary_frac = splitter(deci)
+    #-------------SIGNED HEXADECIMAL-----
+    he_binary_whole, he_binary_frac = splitter(binary)
     he_whole_container = []
     hexa = ""
     for i in range(1, len(he_binary_whole) + 1):
@@ -76,7 +66,7 @@ def signed_deci(value):
             he_whole_container.append(hexa)
             hexa = ""
         hexa = str(he_binary_whole[-i]) + hexa
-
+    
     if hexa:
         for _ in range(4):
             if len(hexa) == 4:
@@ -90,8 +80,6 @@ def signed_deci(value):
         convert = hex_replacer(int(y))
         hexa_whole = str(convert) + hexa_whole
     
-    #return hexa_whole
-        
     hex_fraction_container = []
     hex_frac = ""
     for j in he_binary_frac:
@@ -99,7 +87,7 @@ def signed_deci(value):
             hex_fraction_container.append(hex_frac)
             hex_frac = ""
         hex_frac += str(j)
-    
+
     if hex_frac:
         for _ in range(4):
             if len(hex_frac) == 4:
@@ -114,15 +102,7 @@ def signed_deci(value):
         hexa_fraction += str(convert1)
     
     print(f"HEXADECIMAL: {hexa_whole + hexa_fraction}")
-    
-
-    #return oc_binary_whole, oc_binary_frac, he_binary_whole, he_binary_frac
 
 
 
-signed_deci(user)
-
-    
-    
-
-
+signed_binary(user)
