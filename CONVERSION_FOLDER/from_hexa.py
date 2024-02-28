b504d441b1c4b20b75190f35228b83cd1eddee91
JovTim <202280218@psu.palawan.edu.ph>
2024-02-28 15:52:52 +0800

    finished all signed convertion

--- CONVERSION_FOLDER/from_hexa.py
+++ CONVERSION_FOLDER/from_hexa.py
@@ -1,4 +1,4 @@
-from splitter import hexasplitter
+from splitter import hexasplitter, splitter
 from twos_complement import final_phase
 from binary_to_decimal import binary_to_decimal
 
@@ -63,7 +63,50 @@ def signed_hex(value):
     print(f"DECIMAL: -{decimal}")
 
     #-------------SIGNED OCTAL----------------
+    oc_binary_whole, oc_binary_frac = splitter(binary)
+    oc_whole_container = []
+    oc = ""
+    for i in range(1, len(oc_binary_whole) + 1):
+        if len(oc) == 3:
+            oc_whole_container.append(oc)
+            oc = ""
+        oc = str(oc_binary_whole[-i]) + oc 
+    
+    if oc:
+        for _ in range(3):
+            if len(oc) == 3:
+                oc_whole_container.append(oc)
+                break
+            oc = '1' + oc
+
+    octal_whole = ""
+    for i in oc_whole_container:
+        y = binary_to_decimal(i)
+        octal_whole = str(y) + octal_whole
+
+    oc_fraction_container = []
+    oc_frac = ""
+    for j in oc_binary_frac:
+        if len(oc_frac) == 3:
+            oc_fraction_container.append(oc_frac)
+            oc_frac = ""
+        oc_frac += str(j)
+    
+    if oc_frac:
+        for _ in range(3):
+            if len(oc_frac) == 3:
+                oc_fraction_container.append(oc_frac)
+                break
+            oc_frac += '0'
+    
+    octal_fraction = "."
+    for j in oc_fraction_container:
+        a = binary_to_decimal(j)
+        octal_fraction += str(a)
+
+    print(f"OCTAL: 77777777{octal_whole + octal_fraction}")
+
+
 
     
-
-print(signed_hex(user))
+signed_hex(user)
